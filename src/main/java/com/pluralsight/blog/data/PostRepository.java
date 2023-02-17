48a8a1663b17070a93419e18e199dee53afced03
Jerome Moussalli <jmoussalli@sqli.com>
2023-02-17 14:51:27 +0100

    Add Repository annotation on PostRepository interface

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -3,9 +3,12 @@ package com.pluralsight.blog.data;
 import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 //    @RestResource(rel="contains-title", path="containsTitle")
     public List<Post> findByTitleContaining(String title);
